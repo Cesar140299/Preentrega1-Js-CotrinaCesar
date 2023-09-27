b02ab6780b6f84f6e032ee80c760e9b21c477a2c
Cesar Cotrina <cesarcotrina14@gmail.com>
2023-09-27 19:18:21 -0300

    Condicionales y Bucle terminado

---
+++ app.js
@@ -0,0 +1,30 @@
+let cliente = prompt ("Eres Cliente? colocar si o no")
+const contrasenia = 12345
+
+
+
+if (cliente == "si") {
+    alert("Bienvenido")
+    let contrasenia_ingresada = Number(prompt("Ingrese su contraseña"))
+    if ((contrasenia==contrasenia_ingresada) && (contrasenia_ingresada != "")) {
+        alert("Ingreso Exitoso.")
+    } else {
+        while (contrasenia_ingresada!==contrasenia) {
+            contrasenia_ingresada = Number(prompt("Contraseña incorrecta, intente de nuevo"))
+        }   //  Como podria hacer lo mismo pero con un ciclo for?
+    }
+} else if (cliente =="no"){
+    let deseo_de_registro = prompt("Desea Registrarse? Coloque si o no")
+    if (deseo_de_registro=="si") {
+        let nombre_usuario = prompt("Coloque su nombre de usuario")
+        prompt("Coloque su contraseña")
+        alert(`Usuario Registrado. Bienvenido ${nombre_usuario}` )
+
+    } else {
+        alert("Entendido, hasta luego")
+    }
+} else {
+    alert("Esa opcion no existe")
+}
+
+
